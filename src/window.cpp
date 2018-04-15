dea3a05b8b8a0439e8760d94c83b5c2e8b24b17d
Ryan Li <ryanli80@hawaii.edu>
2018-04-14 14:39:00 -1000

    Additional tests

--- src/window.cpp
+++ src/window.cpp
@@ -2,20 +2,29 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window", sf::Style::Close | sf::Style::Titlebar);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window", sf::Style::Close | sf::Style::Titlebar);
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-    }
-
-    return 0;
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+				case sf::Event::Resized:
+					printf("New window width: %i New window height: %i \n", event.size.width, event.size.height);	
+				case sf::Event::TextEntered:
+					if (event.text.unicode < 128){
+						printf("%c", event.text.unicode);
+					}			
+			}
+		}
+	}
+	return 0;
 }
+
