eb9a1799735b05a22c549b5de2fe960fa7cd8bac
jygh98 <jyim3@hawaii.edu>
2018-04-17 17:10:27 -1000

    fds

--- src/window.cpp
+++ src/window.cpp
@@ -1,15 +1,120 @@
 #include <SFML/Graphics.hpp>
+#include <list>
+
+#define MAX_X 1920
+#define MAX_Y 1080
+
+class animation{
+  public:
+  	float Frame;
+    float speed;
+  	sf::Sprite sprite;
+    std::vector<sf::IntRect> frames;
+
+    animation(){}
+    animation(sf::Texture &t, int x, int y, int w, int h, int count, float Speed){
+  	  Frame = 0;
+      speed = Speed;
+
+  		for (int i=0;i<count;i++)
+        frames.push_back(sf::IntRect(x+i*w, y, w, h));
+
+  		sprite.setTexture(t);
+      //sprite.setSmooth(true);
+  		sprite.setOrigin(w/2,h/2);
+          sprite.setTextureRect(frames[0]);
+  	}
+
+
+  	void update(){
+      Frame += speed;
+  		int n = frames.size();
+  		if (Frame >= n) Frame -= n;
+  		if (n>0) sprite.setTextureRect( frames[int(Frame)] );
+  	}
+
+  	bool isEnd(){
+  	  return Frame+speed>=frames.size();
+	   }
+};
+
+class entity{
+  public:
+    float x;
+    float y;
+    float dx;
+    float dy;
+    float R;
+    bool isAlive;
+    std::string name;
+    animation anim;
+
+    entity(){
+      isAlive = 1;
+    }
+
+    void settings(animation &a,int X,int Y,int radius){
+      anim = a;
+      x=X;
+      y=Y;
+      R = radius;
+    }
+
+    virtual void update(){};
+
+    void draw(sf::RenderWindow &app){
+      anim.sprite.setPosition(x,y);
+      app.draw(anim.sprite);
+
+      sf::CircleShape circle(R);
+      circle.setPosition(x,y);
+      circle.setOrigin(R,R);
+    }
+
+    virtual ~entity(){};
+};
+
+class head: public entity{
+public:
+  bool isOutofMap = 0;
+  head(){
+    dx=2;
+    dy=0;
+    name = "head";
+  }
+
+  void update(){
+   x+=dx;
+   y+=dy;
+   if(abs(x-MAX_X) < 1){
+    isOutofMap = 1;
+    dx*=-1;
+  }
+
+  }
+};
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Banana Defense", sf::Style::Close | sf::Style::Titlebar);
+	sf::RenderWindow window(sf::VideoMode(MAX_X, MAX_Y), "Banana Defense", sf::Style::Close | sf::Style::Titlebar);
 
 	sf::Texture t1, t2, t3;
 	t1.loadFromFile("images/Turret.png");
 	t2.loadFromFile("images/map.png");
-	t3.loadFromFile("images/manloloyo_head.png");
+	//t3.loadFromFile("images/manloloyo_head.png");
+  t3.loadFromFile("images/manloloyo.png");
 
-	sf::Sprite sTurret(t1), sBackground(t2), sHead(t3);
+	sf::Sprite sTurret(t1), sBackground(t2);
+
+  animation sHead(t3, 0, 0, 500, 500, 10000, 0.00001);
+
+  std::list<entity*> entities;
+  for(int i=0;i<1;i++)
+  {
+    head *h = new head();
+    h->settings(sHead, 500, 500, 100);
+    entities.push_back(h);
+  }
 
 	bool isMove=false;
 	float dx=0, dy=0;
@@ -40,21 +145,23 @@ int main()
 				if(event.key.code == sf::Mouse::Left)
 					isMove=false;
 			}
-
-			if(event.type == sf::Event::Resized)
-					printf("New window width: %i New window height: %i \n", event.size.width, event.size.height);
-
-			if(event.type == sf::Event::TextEntered){
-					if (event.text.unicode < 128)
-						printf("%c", event.text.unicode);
-			}
 			if (isMove) sTurret.setPosition(pos.x-dx, pos.y-dy);
 		}
+    for(auto i=entities.begin();i!=entities.end();){
+      entity *e = *i;
+
+      e->update();
+      e->anim.update();
+
+      if (e->isAlive==false) {i=entities.erase(i); delete e;}
+      else i++;
+    }
 		// draw
 		window.clear();
 		window.draw(sBackground);
 		window.draw(sTurret);
-		window.draw(sHead);
+    for(auto i:entities)
+      i->draw(window);
 		window.display();
 	}
 	return 0;
