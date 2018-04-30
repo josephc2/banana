8c36a73b4fa42d6482200ccf8e92166ffd4974fc
Ryan Li <ryanli80@hawaii.edu>
2018-04-29 23:40:10 -1000

    added bullets

--- src/turret.cpp
+++ src/turret.cpp
@@ -1,13 +1,61 @@
 class turret: public entity{
 	public:
 		turret(){
-      dx = 0;
-      dy = 0;
+			dx = 0;
+			dy = 0;
 			name = "Turret";
 		}
 
 		void update(){
 			x+=dx;
 			y+=dy;
+		}
+};
+class Bullet: public entity{
+	public:
+		int firing = 0;
+		int count  = 0;
+		Bullet(){
+			dx = 0;
+			dy = 0;
+			name = "bullet";
+		}
+		void update(){
+			x+=dx;
+			y+=dy;
+			if(count == 10){
+				dx = 0;
+				dy = -20;
+				count = 0;
+				//firing = 1;
+			}
+			else {
+				count++;
+				dx = 0;
+				dy = 0;
+			}
+		}
+};
+class BulletUp: public Bullet{
+	public:
+		BulletUp(){
+			dx = 0;
+			dy = 0;
+			name = "bulletUp";
+		}
+		void update(){
+			x+=dx;
+			y+=dy;
+			if(count == 10){
+				dx = 0;
+				dy = 20;
+				count = 0;
+				//firing = 1;
+			}
+			else {
+				count++;
+				dx = 0;
+				dy = 0;
+			}
 		}
 };
--- src/window.cpp
+++ src/window.cpp
@@ -11,9 +11,9 @@
 #define MAX_Y 1080
 
 bool isCollide(entity *a,entity *b){
-  return (b->x - a->x)*(b->x - a->x)+
-         (b->y - a->y)*(b->y - a->y)<
-         (a->R + b->R)*(a->R + b->R);
+	return (b->x - a->x)*(b->x - a->x)+
+		(b->y - a->y)*(b->y - a->y)<
+		(a->R + b->R)*(a->R + b->R);
 }
 
 int main()
@@ -21,21 +21,21 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(MAX_X, MAX_Y), "Banana Defense", sf::Style::Close | sf::Style::Titlebar);
 	window.setFramerateLimit(60);
 
-	sf::Texture t0, t1, t2, t3, t4, t5, t6, t7;
-  sf::Font font;
-  font.loadFromFile("font/big_noodle_titling.ttf");
+	sf::Texture t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
+	sf::Font font;
+	font.loadFromFile("font/big_noodle_titling.ttf");
 
-  // for bank system
-  sf::Text text("100", font);
-  text.setCharacterSize(100);
-  text.setColor(sf::Color::White);
-  text.setPosition(1700,820);
+	// for bank systems
+	sf::Text text("100", font);
+	text.setCharacterSize(100);
+	text.setColor(sf::Color::White);
+	text.setPosition(1700,820);
 
-  // for life system
-  sf::Text live("10", font);
-  live.setCharacterSize(100);
-  live.setColor(sf::Color::White);
-  live.setPosition(1350,850);
+	// for life system
+	sf::Text live("10", font);
+	live.setCharacterSize(100);
+	live.setColor(sf::Color::White);
+	live.setPosition(1350,850);
 
 	t0.loadFromFile("images/Turret.png");
 	t1.loadFromFile("images/Turret.png");
@@ -44,29 +44,40 @@ int main()
 	t4.loadFromFile("images/title.jpg");
 	t5.loadFromFile("images/banner.png");
 	t6.loadFromFile("images/tep.png");
-  t7.loadFromFile("images/banana-hi.png");
+	t7.loadFromFile("images/banana-hi.png");
+	t8.loadFromFile("images/bullet.png");
+	t9.loadFromFile("images/bullet.png");
 
 	sf::Sprite sTurret(t1), sTurretBackup(t0), sBackground(t2), sTitle(t4), sBanner(t5), sBanana(t7);
 	sTurret.setPosition(1650, 250);
 	sTurretBackup.setPosition(1650, 250);
 	sBanner.setPosition(525,500);
-  sBanana.setPosition(1350,860);
+	sBanana.setPosition(1350,860);
+
 
 	animation sHead(t3, 0, 0, MAX_X/4, MAX_Y/4, 10000, 0.00001);
 	animation sBossHead(t6, 0, 0, MAX_X/4, MAX_Y/4, 10000, 0.00001);
 	animation sTurretPlaced(t1, 0, 0, MAX_X/4, MAX_Y/4, 10000, 0.00001);
+	animation sBulletshoot(t8, 0, 0, MAX_X/4, MAX_Y/4, 10000, 0.00001);
 
 	std::list<entity*> entities;
 	bool gameStart=false;
 	bool gamePause=true;
 	bool isMove=false;
+	bool fire=false;
 	float dx=0, dy=0;
 	int count = 0;
 	int numHeads = 0;
 	int waveMode = 0;
 	int liveCheck = 0;
 	int lifeleft = 10;
-  int bank = 100;
+	int bank = 500;
+	int counter = 0;
+	int numBullet = 0;
+	int turretPlace = 0;
+	int interate = 0;
+	float locX[100], locY[100];
+	//float locX=0, locY=0;
 
 	// run the program as long as the window is open
 	while (window.isOpen()){
@@ -99,18 +110,35 @@ int main()
 				// printf("Button was released\n");
 				if(event.key.code == sf::Mouse::Left)
 					isMove=false;
-					// std::cout << "Turret x position: " << sTurret.getPosition().x << std::endl;
-					// std::cout << "Turret y position: " << sTurret.getPosition().y << std::endl;
-          if(bank >= 100){
-            turret *t = new turret();
-            t->settings(sTurretPlaced, sTurret.getPosition().x + 240, sTurret.getPosition().y + 135, 100);
-            entities.push_back(t);
-            bank-=100;
-          }
+				// std::cout << "Turret x position: " << sTurret.getPosition().x << std::endl;
+				// std::cout << "Turret y position: " << sTurret.getPosition().y << std::endl;
+				if(bank >= 100){
+					turret *t = new turret();
+					t->settings(sTurretPlaced, sTurret.getPosition().x + 240, sTurret.getPosition().y + 135, 100);
+					entities.push_back(t);
+					bank-=100;
 					sTurret.setPosition(1650, 250);
+				}
+				//sTurret.setPosition(1650, 250);
 			}
-			if (isMove) sTurret.setPosition(pos.x-dx, pos.y-dy);
+
+			if (isMove){
+				sTurret.setPosition(pos.x-dx, pos.y-dy);
+				if(interate != 1000){
+					interate++;
+				}
+				else{
+					turretPlace++;
+					for(int i =0; i < turretPlace; i++){
+						locX[i] =  sTurret.getPosition().x;
+						locY[i] =  sTurret.getPosition().y;
+					}
+					interate =0;
+				}
+				std::cout <<"Count" <<std::endl;
+			}
 		}
+
 		// draw loading screen
 		if(!gameStart){
 			window.clear();
@@ -119,6 +147,32 @@ int main()
 		}
 		// actual game
 		if(gameStart){
+
+			if(counter < 100){
+				if(fire = true){
+					//top bullet
+					Bullet *b = new Bullet();
+					for(int i =0; i < turretPlace; i++){
+						b->settings(sBulletshoot, locX[i] + 100, locY[i] - 80, 10);
+						entities.push_back(b);
+					}
+					std::cout << "Placed Turret" <<std::endl;
+					//bottom bullet
+					BulletUp *b1 = new BulletUp();
+					for(int i =0; i < turretPlace; i++){
+						b1->settings(sBulletshoot, locX[i] + 100, locY[i] + 80, 10);
+						entities.push_back(b1);
+					}
+					std::cout << "Placed" <<std::endl;
+					fire = false;
+					counter = 0;
+				}
+				counter++;
+			}
+			else
+				fire = true;
+
+
 			// wave 0
 			if(waveMode == 0 && !gamePause){
 				if(numHeads < 5){
@@ -166,10 +220,10 @@ int main()
 					i=entities.erase(i);
 					delete e;
 					lifeleft--;
-          // ##############################################
-          // remember to remove this for full game
-          bank += 100;
-          // #############################################
+					// ##############################################
+					// remember to remove this for full game
+					bank += 100;
+					// #############################################
 					std::cout << "You have " << lifeleft << "lives left" << std::endl;
 				}
 				else i++;
@@ -179,15 +233,16 @@ int main()
 			window.draw(sBackground);
 			window.draw(sTurret);
 			window.draw(sTurretBackup);
-      window.draw(sBanana);
+			window.draw(sBanana);
 
-      // update money in the bank
-      text.setString(std::to_string(bank));
-      window.draw(text);
 
-      // update number of lives left
-      live.setString(std::to_string(lifeleft));
-      window.draw(live);
+			// update money in the bank
+			text.setString(std::to_string(bank));
+			window.draw(text);
+
+			// update number of lives left
+			live.setString(std::to_string(lifeleft));
+			window.draw(live);
 
 			if(gamePause)
 				window.draw(sBanner);
